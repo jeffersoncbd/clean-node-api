f17370a233b28b8be6e4e3b279048ff3f7afcb23
Jefferson Carlos <jeff.carlosbd@gmail.com>
2020-12-14 17:41:17 +0000

    chore: adiciona jest

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+export default {
+  roots: ['<rootDir>/src'],
+
+  clearMocks: true,
+
+  collectCoverage: false,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+
+  testEnvironment: 'node',
+
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
