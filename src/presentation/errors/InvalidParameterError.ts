39d5f11aa768355acabe5031a5863f45b9da9c65
Jefferson Carlos <jeff.carlosbd@gmail.com>
2020-12-15 12:38:04 +0000

    chore: cria protocolo de validação de email e erro para parametros inválidos

---
+++ src/presentation/errors/InvalidParameterError.ts
@@ -0,0 +1,6 @@
+export class InvalidParameterError extends Error {
+  constructor(parameterName: string) {
+    super(`Invalid parameter: ${parameterName}`)
+    this.name = 'InvalidParameterError'
+  }
+}
