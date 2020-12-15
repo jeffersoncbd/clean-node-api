a8d5158e4a6e3e384f2855b537cbb840cc7189ca
Jefferson Carlos <jeff.carlosbd@gmail.com>
2020-12-15 11:45:06 +0000

    refactor: move codigo duplicado para uma função helper

--- src/presentation/controllers/SignUp.ts
+++ src/presentation/controllers/SignUp.ts
@@ -1,14 +1,15 @@
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { MissingParameterError } from '../errors/MissingParameterError'
+import { badRequest } from '../helpers/httpHelpers'
 
 export class SignUpController {
   handle(httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
-      return { statusCode: 400, body: new MissingParameterError('name') }
+      return badRequest(new MissingParameterError('name'))
     }
 
     if (!httpRequest.body.email) {
-      return { statusCode: 400, body: new MissingParameterError('email') }
+      return badRequest(new MissingParameterError('email'))
     }
 
     return { statusCode: 200, body: '' }
