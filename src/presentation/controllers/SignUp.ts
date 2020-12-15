1ee687bcc4cbc93f781038500482b6d700b8c520
Jefferson Carlos <jeff.carlosbd@gmail.com>
2020-12-15 13:14:49 +0000

    feat: adiciona try catch para lidar com erros de dependencias

--- src/presentation/controllers/SignUp.ts
+++ src/presentation/controllers/SignUp.ts
@@ -4,23 +4,33 @@ import { badRequest } from '../helpers/httpHelpers'
 import { Controller } from '../protocols/controller'
 import { EmailValidator } from '../protocols/emailValidator'
 import { InvalidParameterError } from '../errors/InvalidParameterError'
+import { ServerError } from '../errors/ServerError'
 
 export class SignUpController implements Controller {
   constructor(private emailValidator: EmailValidator) {}
 
   handle(httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParameterError(field))
+    try {
+      const requiredFields = [
+        'name',
+        'email',
+        'password',
+        'passwordConfirmation'
+      ]
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParameterError(field))
+        }
       }
-    }
 
-    const emailIsValid = this.emailValidator.isValid(httpRequest.body.email)
-    if (!emailIsValid) {
-      return badRequest(new InvalidParameterError('email'))
-    }
+      const emailIsValid = this.emailValidator.isValid(httpRequest.body.email)
+      if (!emailIsValid) {
+        return badRequest(new InvalidParameterError('email'))
+      }
 
-    return { statusCode: 200, body: '' }
+      return { statusCode: 200, body: '' }
+    } catch (error) {
+      return { statusCode: 500, body: new ServerError() }
+    }
   }
 }
