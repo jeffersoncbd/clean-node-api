c9f8ff33f554b5b86f9211fd573975c95552f379
Jefferson Carlos <jeff.carlosbd@gmail.com>
2020-12-16 12:55:52 +0000

    test: faz o EmailValidatorAdapter validar o email recebido utilizando a biblioteca validator

--- src/utils/emailValidator/adapter.spec.ts
+++ src/utils/emailValidator/adapter.spec.ts
@@ -1,4 +1,11 @@
 import { EmailValidatorAdapter } from './Adapter'
+import validator from 'validator'
+
+jest.mock('validator', () => ({
+  isEmail(): boolean {
+    return true
+  }
+}))
 
 function makeSystemUnderTest() {
   const systemUnderTest = new EmailValidatorAdapter()
@@ -8,8 +15,17 @@ function makeSystemUnderTest() {
 describe('EmailValidatorAdapter', () => {
   test('Deve retornar "false" se emailValidator retornar "false"', () => {
     const systemUnderTest = makeSystemUnderTest()
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
+
     const isValid = systemUnderTest.isValid('invalid_email@mail.com')
 
     expect(isValid).toBe(false)
+  })
+
+  test('Deve retornar "true" se emailValidator retornar "true"', () => {
+    const systemUnderTest = makeSystemUnderTest()
+    const isValid = systemUnderTest.isValid('valid_email@mail.com')
+
+    expect(isValid).toBe(true)
   })
 })
