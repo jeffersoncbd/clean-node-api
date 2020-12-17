f7406f0b9e11ef8c3bd4b72f90e917ffc7cd1670
Jefferson Carlos <jeff.carlosbd@gmail.com>
2020-12-17 22:27:20 +0000

    test: faz o AccountMongoDBRepository retornar os dados da conta criada

---
+++ src/infrastructure/database/mongodb/repositories/Account.spec.ts
@@ -0,0 +1,35 @@
+import { mongoConnectionHelper } from '../helpers/connection'
+import { AccountMongoDBRepository } from './Account'
+
+function makeSystemUnderTest() {
+  const systemUnderTest = new AccountMongoDBRepository()
+  return { systemUnderTest }
+}
+
+describe('AccountMongoDBRepository', () => {
+  beforeAll(async () => {
+    if (!process.env.MONGO_URL) {
+      throw new Error('MONGO_URL não está disponível')
+    }
+    await mongoConnectionHelper.connect(process.env.MONGO_URL)
+  })
+  afterAll(async () => {
+    await mongoConnectionHelper.disconnect()
+  })
+
+  test('Deve retornar a conta criada quando ter sucesso', async () => {
+    const { systemUnderTest } = makeSystemUnderTest()
+
+    const account = await systemUnderTest.create({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_email@mail.com')
+    expect(account.password).toBe('any_password')
+  })
+})
