c846f79cf2e6bf8c149166d606c18b451c1fa482
Jefferson Carlos <jeff.carlosbd@gmail.com>
2020-12-17 11:54:50 +0000

    test: faz o DatabaseCreateAccountUseCase utilizar o CreateAccountRepository

--- src/data/usecases/Database/CreateAccount.spec.ts
+++ src/data/usecases/Database/CreateAccount.spec.ts
@@ -1,5 +1,9 @@
 import { DatabaseCreateAccountUseCase } from './CreateAccount'
-import { Encrypter } from './CreateAccount.protocols'
+import {
+  Encrypter,
+  AccountEntity,
+  CreateAccountRepository
+} from './CreateAccount.protocols'
 
 function makeEncrypterStub() {
   class EncrypterStub implements Encrypter {
@@ -10,10 +14,29 @@ function makeEncrypterStub() {
   return new EncrypterStub()
 }
 
+function makeCreateAccountRepositoryStub() {
+  class CreateAccountRepositoryStub implements CreateAccountRepository {
+    async create(): Promise<AccountEntity> {
+      const fakeAccount: AccountEntity = {
+        id: 'valid_id',
+        name: 'valid_name',
+        email: 'valid_email',
+        password: 'hashed_password'
+      }
+      return fakeAccount
+    }
+  }
+  return new CreateAccountRepositoryStub()
+}
+
 function makeSystemUnderTest() {
   const encrypterStub = makeEncrypterStub()
-  const systemUnderTest = new DatabaseCreateAccountUseCase(encrypterStub)
-  return { systemUnderTest, encrypterStub }
+  const createAccountRepositoryStub = makeCreateAccountRepositoryStub()
+  const systemUnderTest = new DatabaseCreateAccountUseCase(
+    encrypterStub,
+    createAccountRepositoryStub
+  )
+  return { systemUnderTest, encrypterStub, createAccountRepositoryStub }
 }
 
 describe('DatabaseCreateAccountUseCase', () => {
@@ -43,5 +66,25 @@ describe('DatabaseCreateAccountUseCase', () => {
     })
 
     await expect(promise).rejects.toThrow()
+  })
+
+  test('Deve chamar CreateAccountRepository com a senha correta', async () => {
+    const {
+      systemUnderTest,
+      createAccountRepositoryStub
+    } = makeSystemUnderTest()
+    const createSpy = jest.spyOn(createAccountRepositoryStub, 'create')
+
+    await systemUnderTest.create({
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    })
+
+    expect(createSpy).toHaveBeenCalledWith({
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'hashed_password'
+    })
   })
 })
