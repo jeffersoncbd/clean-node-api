707ec8d302382a041c46e31befa80a6a5a6a3a30
Jefferson Carlos <jeff.carlosbd@gmail.com>
2020-12-18 15:26:16 +0000

    feat: cria arquivo para variaveis de ambiente e atualiza no arquivo de entrada da aplicação

--- src/main/application.ts
+++ src/main/application.ts
@@ -1,5 +1,20 @@
 import { expressServer } from './server'
+import { mongoConnectionHelper } from '../infrastructure/database/mongodb/helpers/connection'
+import { env } from './env'
 
-expressServer.listen(3001, () =>
-  console.log(`Servidor iniciado na porta ${3001}`)
-)
+async function root() {
+  try {
+    await mongoConnectionHelper.connect(env.mongoUrl)
+
+    expressServer.listen(env.serverPort, () =>
+      console.log(`Servidor iniciado na porta ${env.serverPort}`)
+    )
+  } catch (error) {
+    console.log('[application.ts] - ERROR')
+    console.log()
+    console.error(error)
+    console.log()
+  }
+}
+
+root()
---
+++ src/main/env.ts
@@ -0,0 +1,8 @@
+console.log(process.env.MONGO_URL)
+
+export const env = {
+  serverPort: process.env.SERVER_PORT || 3001,
+  mongoUrl:
+    process.env.MONGO_URL ||
+    'mongodb://emnuvem:JhlJOOWVKpx9Yro@db.emnuvem.co:35023/clean-node-api_DEV'
+}
