3f2871798243cf06bb0cc5b7f65eb355c528f086
Jefferson Carlos <jeff.carlosbd@gmail.com>
2020-12-18 20:03:34 +0000

    feat: corrige conexao local mongo

--- src/main/env.ts
+++ src/main/env.ts
@@ -1,6 +1,5 @@
 export const env = {
   serverPort: process.env.SERVER_PORT || 3001,
   mongoUrl:
-    process.env.MONGO_URL ||
-    'mongodb://emnuvem:JhlJOOWVKpx9Yro@db.emnuvem.co:35023/clean-node-api_DEV'
+    process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api_DEV'
 }
